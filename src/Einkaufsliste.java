6bd09cb4d8cc1a58b70a2ea1a0079e859bdffebc
lunak <luna.kreyenberg@gmail.com>
2025-02-12 13:57:31 +0100

    Add Einkaufsliste class and initial project setup

---
+++ src/Einkaufsliste.java
@@ -0,0 +1,57 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Einkaufsliste {
+    private ArrayList<String> einkaufsliste = new ArrayList<String>();
+
+    public void useListener() {
+        String input;
+        boolean running = true;
+        while (running) {
+            System.out.print("> ");
+            input = System.console().readLine();
+            if (input.equals("exit")|| input.equals("Exit")) {
+                running = false;
+            } else if (input.equals("list")|| input.equals("List")) {
+                getList();
+            } else if (input.startsWith("add") || input.startsWith("Add")) {
+                addToList(input.substring(3));
+            } else if (input.startsWith("remove") || input.startsWith("Remove")) {
+                removeFromList(input.substring(6));
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+
+    public void addToList(String item) {
+        if(item.isEmpty()) {
+            System.out.println("Please enter an item to add!");
+        } else {
+            einkaufsliste.add(item);
+            System.out.println("Added " + item + " to the list.");
+        }
+    }
+
+    public void getList() {
+        if(einkaufsliste.isEmpty()) {
+            System.out.println("Your list is empty!");
+        } else {
+            System.out.println("~~~ Shopping List ~~~");
+            for (String item : einkaufsliste) {
+                System.out.print("- ");
+                System.out.println(item);
+            }
+        }
+
+    }
+
+    public void removeFromList(String item) {
+        if(item.isEmpty()) {
+            System.out.println("Please enter an item to remove!");
+        } else {
+            einkaufsliste.remove(item);
+            System.out.println("Removed " + item + " from the list.");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Einkaufsliste einkaufsliste = new Einkaufsliste();
+        einkaufsliste.useListener();
+    }
+}
